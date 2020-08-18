642bd31c040004e8ec066dba5134483f6c8e02cb
Tiffany Abraham <tony.calablaval@gmail.com>
2020-08-18 07:36:42 -0400

    Color scheme modified to neon pink, purple, and teal

--- colors.js
+++ colors.js
@@ -1,3 +1,4 @@
+// colors.js
 module.exports = {
   background: '#FFFFFF',
   backgroundDark: '#f0e6f6',
@@ -6,9 +7,24 @@ module.exports = {
 
   primary: '#7c37ad',
   primaryLight: '#ae66df',
-  primaryDark: '#4b007d',
+  primaryDark: '#ff4081',
 
-  secondary: '#ff4081',
+  secondary: '#05dfd7',
   secondaryLight: '#ff79b0',
-  secondaryDark: '#c60055',
+  secondaryDark: '#fa26a0',
 };
+
+// module.exports = {
+//   background: '#FFFFFF',
+//   backgroundDark: '#f0e6f6',
+//
+//   text: '#333333',
+//
+//   primary: '#7c37ad',
+//   primaryLight: '#ae66df',
+//   primaryDark: '#4b007d',
+//
+//   secondary: '#ff4081',
+//   secondaryLight: '#faeee7',
+//   secondaryDark: '#c60055',
+// };
